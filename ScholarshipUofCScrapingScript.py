bdeb1bce807770aa3dd172a252ff3cb551945ac8
Abhay2412 <abhaykhosla0@gmail.com>
2023-11-14 17:58:01 -0700

    Finish getting 1500 scholarships from the University of Calgary website

--- ScholarshipUofCScrapingScript.py
+++ ScholarshipUofCScrapingScript.py
@@ -4,36 +4,45 @@ import json
 import re
 import csv
 
+
 def fetch_scholarship_links(list_page_url):
     response = requests.get(list_page_url)
     if response.status_code != 200:
-        return []
+        return set()
 
     soup = BeautifulSoup(response.content, 'html.parser')
-    
     scholarship_links = soup.select('.award-tile a')
     unique_links = set()
     base_url = "https://ucalgary.ca"
 
     for link in scholarship_links:
         href = link.get('href')
-        if href and not href.startswith('http'):
-            full_url = base_url + href
-        else:
-            full_url = href
+        full_url = base_url + href if href and not href.startswith('http') else href
         unique_links.add(full_url)
 
-    return list(unique_links)
+    return unique_links
+
+
+def fetch_all_scholarship_links(base_url, max_pages):
+    all_links = set()
+    for i in range(max_pages):
+        page_url = f'{base_url}?page={i}'
+        all_links.update(fetch_scholarship_links(page_url))
+    return all_links
+
 
 def safe_get_text(element):
     return element.get_text(strip=True) if element else "N/A"
 
+
 def extract_int(text):
     return int(re.findall(r'\d+', text.replace(',', ''))[0]) if text and re.search(r'\d+', text) else 0
+
 
 def year_label_to_int(year_label):
     year_match = re.search(r'(\d+)(?:st|nd|rd|th)', year_label)
     return int(year_match.group(1)) if year_match else 0
+
 
 def scrape_scholarship(url):
     response = requests.get(url)
@@ -74,6 +83,7 @@ def scrape_scholarship(url):
 
     return scholarship
 
+
 def convert_to_csv(all_scholarships):
     csv_columns = ["title", "award_value", "number_of_awards", "competition_to_apply", "donor", "award_description", "required_criteria", "year_entering", "award_information"]
     
@@ -90,14 +100,16 @@ def convert_to_csv(all_scholarships):
 
 # Base URL of the website where scholarships are listed
 base_url = "https://ucalgary.ca/registrar/finances/awards-scholarships-and-bursaries/search-awards"
-scholarship_links = fetch_scholarship_links(base_url)
-all_scholarships = [scrape_scholarship(url) for url in scholarship_links]
 
-# Sort the scholarships alphabetically by title
+max_pages = 30  
+scholarship_links = fetch_all_scholarship_links(base_url, max_pages)
+
+all_scholarships = [scrape_scholarship(url) for url in scholarship_links]
 all_scholarships_sorted = sorted(all_scholarships, key=lambda x: x['title'])
 
+# Write to JSON file
 with open('scholarships_data_sorted.json', 'w', encoding='utf-8') as f:
     json.dump(all_scholarships_sorted, f, ensure_ascii=False, indent=4)
 
-# Write data to CSV file
-convert_to_csv(all_scholarships)
+# Write to CSV file
+convert_to_csv(all_scholarships_sorted)
