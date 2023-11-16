b99e3c4ee47b4a0d337d1fdf67808f2226cd1b4b
Abhay2412 <abhaykhosla0@gmail.com>
2023-11-15 18:48:33 -0700

    fix: Update the color scheme and also add in Flashcard UI placeholder

--- frontend/src/pages/SummarizerPage.jsx
+++ frontend/src/pages/SummarizerPage.jsx
@@ -17,24 +17,28 @@ const SummarizerPage = () => {
     }
 
     return (
-        <div className="App" style={{ display: "flex", flexDirection: "row", height: "100vh" }}>
-            <div style={{ flex: 1, overflowY: "scroll" }}>
-                <input type="file" onChange={onFileChange} accept="application/pdf" />
-                {file && (
-                    <Document
-                        file={file}
-                        onLoadSuccess={onDocumentLoadSuccess}
-                    >
-                        {Array.from(new Array(numPages), (el, index) => (
-                            <Page key={`page_${index + 1}`} pageNumber={index + 1} />
-                        ))}
-                    </Document>
-                )}
+        <div className="App" style={{ display: "flex", flexDirection: "row", height: "100vh", backgroundColor: "white" }}>
+            <div style={{ flex: 1, display: "flex", flexDirection: "column", alignItems: "center"}}>
+                <div style={{ overflowY: "scroll", height: "50vh", width: "90%", scrollbarWidth: "thin" }}>
+                    <input type="file" onChange={onFileChange} accept="application/pdf" />
+                    {file && (
+                        <Document
+                            file={file}
+                            onLoadSuccess={onDocumentLoadSuccess}
+                        >
+                            {Array.from(new Array(numPages), (el, index) => (
+                                <Page key={`page_${index + 1}`} pageNumber={index + 1} />
+                            ))}
+                        </Document>
+                    )}
+                </div>
+                <div style={{ height: "50vh", width: "90%", marginTop: "10px", display: "flex", alignItems: "center", justifyContent: "center" }}>
+                    <p style={{ color: "#B8E4BC" }}>Flashcard UI Placeholder</p>
+                </div>
             </div>
-            <div style={{ flex: 1, backgroundColor: "#f0f0f0", padding: "10px" }}>
-                <h2>Chatbot Interface</h2>
-                {/* Placeholder for chatbot interface */}
-                <textarea style={{ width: "100%", height: "90%" }} placeholder="Chatbot will go here"></textarea>
+            <div style={{ flex: 1, padding: "10px", height: "100vh" }}>
+                <h2 style={{ color: "#3B8070" }}>Chatbot Interface</h2>
+                <textarea style={{ width: "100%", height: "90%", borderColor: "#B8E4BC" }} placeholder="Chatbot will go here"></textarea>
             </div>
         </div>
     );
