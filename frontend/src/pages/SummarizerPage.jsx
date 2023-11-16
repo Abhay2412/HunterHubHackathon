56221e9f8bad3ccd87ddc1448b2b0c18999483d0
Abhay2412 <abhaykhosla0@gmail.com>
2023-11-15 20:16:43 -0700

    feat: Add in the basic implementation of the chatbot to the screen

--- frontend/src/pages/SummarizerPage.jsx
+++ frontend/src/pages/SummarizerPage.jsx
@@ -1,4 +1,6 @@
 import React, { useState } from "react";
+import { ThemeProvider } from 'styled-components';
+import ChatBot from "react-simple-chatbot";
 
 const SummarizerPage = () => {
     const [file, setFile] = useState(null);
@@ -9,6 +11,35 @@ const SummarizerPage = () => {
             setFile(URL.createObjectURL(file));
         }
     }
+
+    const steps = [
+        {
+            id: '1',
+            message: 'Hello! I am your summarizer chatbot. I can help you summarize your PDF notes.',
+            end: true,
+        },
+    ];
+
+    const customStyle = {
+        userBubble: {
+          height: '85%', 
+        },
+        botBubble: {
+          maxWidth: '100%', 
+        },
+      };
+
+    const theme = {
+        background: "#fff",
+        fontFamily: "Arial, Helvetica, sans-serif",
+        headerBgColor: "#B8E4BC",
+        headerFontColor: "#fff",
+        headerFontSize: "15px",
+        botBubbleColor: "#B8E4BC",
+        botFontColor: "#fff",
+        userBubbleColor: "#315234",
+        userFontColor: "#4A4A4A",
+    };
 
     return (
         <div className="App" style={{ display: "flex", flexDirection: "row", height: "100vh", backgroundColor: "white" }}>
@@ -27,9 +58,8 @@ const SummarizerPage = () => {
                     <p style={{ color: "#B8E4BC" }}>Flashcard UI Placeholder</p>
                 </div>
             </div>
-            <div style={{ flex: 1, padding: "10px", height: "100vh" }}>
-                <h2 style={{ color: "#3B8070" }}>Chatbot Interface</h2>
-                <textarea style={{ width: "100%", height: "90%", borderColor: "#B8E4BC" }} placeholder="Chatbot will go here"></textarea>
+            <div style={{ flex: 1, padding: "10px", height: "100vh"}}>
+            <ThemeProvider theme={theme}> <ChatBot steps={steps} style={{ height: '100vh', width: '100%' }} contentStyle={customStyle.userBubble} bubbleStyle={customStyle.botBubble}/> </ThemeProvider>
             </div>
         </div>
     );
